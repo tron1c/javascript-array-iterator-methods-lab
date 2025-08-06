36fcb52eb706f7cb96d5e69b28f71319cf16daa6
tron1c <alaali32@gmail.com>
2025-08-06 12:17:18 +0300

    actualy done

--- app.js
+++ app.js
@@ -304,4 +304,68 @@ isAdultPresent = devs.some(dev => dev.year >= 18);
 console.log('Exercise 6 my result: ', isAdultPresent);
 console.log('Exercise 6 correct result: ', true);
 
- 
+ ///////////////////
+
+ /*
+Exercise 7: Array.prototype.every()
+
+Use Array.prototype.every() to determine if every person in the devs array is 
+19 years old or older.
+
+- You have an array of individuals, each with their year of birth represented
+  by the 'year' property.
+- Use the Array.prototype.every() method to verify if every individual in the
+  array is at least 19 years old.
+- Store the result (true or false) in the variable 'isEveryone19OrOlder'.
+*/
+
+let isEveryone19OrOlder = null;
+
+// Complete the exercise in the space below:
+const currentYear = new Date().getFullYear();
+isEveryone19OrOlder = devs.every(dev => (currentYear - dev.year) >= 19);
+
+
+console.log('Exercise 7 my result: ', isEveryone19OrOlder);
+console.log('Exercise 7 correct result: ', false);
+
+////////////////////////
+
+/*
+Exercise 8: Array.prototype.find()
+
+Use Array.prototype.find() to identify and retrieve the comment object with 
+a specific ID 823423 from an array of comment objects.
+
+- Assign the found comment object to the variable 'commentById'.
+*/
+
+let commentById = {};
+
+// Complete the exercise in the space below:
+commentById = comments.find(comment => comment.id === 823423);
+
+
+console.log('Exercise 8 my result: ', commentById);
+console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 });
+
+////////////////////////
+
+/*
+Exercise 9: Array.prototype.findIndex()
+
+Determine the index of the comment that has a specific ID 123523 in an array 
+of comment objects.
+
+- Store the index in the variable 'idx'.
+*/
+
+let idx = null;
+
+// Complete the exercise in the space below:
+idx = comments.findIndex(comment => comment.id === 123523);
+
+// Check your work:
+console.log('Exercise 9 my result: ', idx);
+console.log('Exercise 9 correct result: ', 3);
+
